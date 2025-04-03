3c838ec5ab9ee6fc5af5bf5707949c01ea76d26a
leosalgado <leonardosalgado@protonmail.com>
2025-04-03 01:18:44 -0300

    Test CSV export using first page

--- 2_data_transform/main.py
+++ 2_data_transform/main.py
@@ -1,3 +1,21 @@
 import pdfplumber
 import pandas as pd
 import zipfile
+import csv
+
+pdf_path = (
+    "../1_webscraping/webscraper/downloads/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
+)
+output_csv = "output.csv"
+
+with pdfplumber.open(pdf_path) as pdf:
+    first_page = pdf.pages[2]
+    table = first_page.extract_table()
+
+with open(output_csv, mode="w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+
+    for row in table:
+        writer.writerow(row)
+
+print(f"Arquivo CSV salvo como: {output_csv}")
